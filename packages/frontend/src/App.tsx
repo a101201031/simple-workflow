ce02819d03df281a122354a06ca1cd4a19ca5f1a
a101201031 <0825tngus@naver.com>
2021-02-04 00:28:17 +0900

    feat: add react-router

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,34 +1,19 @@
 import React from 'react';
-import {
-  Box,
-  Button,
-  ButtonGroup,
-  ChakraProvider,
-  Container,
-  Flex,
-  Spacer,
-} from '@chakra-ui/react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home } from 'Home';
+import { SignUp } from 'SignUp';
 
 export const App = () => {
   return (
-    <ChakraProvider>
-      <Flex mh="64px" mt="auto" bg="gray.700" p="3" alignItems="center">
-        <Box p="2" bg="red.400">
-          LOGO
-        </Box>
-        <Spacer />
-        <Box>
-          <ButtonGroup colorScheme="gray">
-            <Button variant="link" m="0px 15px">
-              Sign in
-            </Button>
-            <Button variant="outline" m="0px 15px">
-              Sign on
-            </Button>
-          </ButtonGroup>
-        </Box>
-      </Flex>
-      <Container></Container>
-    </ChakraProvider>
+    <Router>
+      <Switch>
+        <Route path="/sign-up">
+          <SignUp />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
