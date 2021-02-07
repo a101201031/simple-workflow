4581eafc170a574213f0f225595cac042382b82e
a101201031 <0825tngus@naver.com>
2021-02-07 21:59:31 +0900

    feat: add form with react-hooks-form + yup

--- packages/frontend/src/SignUp.tsx
+++ packages/frontend/src/SignUp.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import {
   Box,
   Button,
@@ -9,8 +9,12 @@ import {
   FormLabel,
   Heading,
   Input,
+  FormErrorMessage,
 } from '@chakra-ui/react';
 import { fetcher } from 'helper';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
 interface AccountSignUpTypes {
   email: string;
@@ -19,26 +23,44 @@ interface AccountSignUpTypes {
   confirmPassword: string;
 }
 
+const schema = Yup.object().shape({
+  email: Yup.string().email(),
+  username: Yup.string()
+    .min(3)
+    .max(20)
+    .matches(
+      /^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$/,
+      'Need characters including only a number and a lower/upper letter.',
+    ),
+  password: Yup.string()
+    .min(8, 'Minimum password length to 8 characters.')
+    .max(15, 'Maximum password length to 15 characters.')
+    .matches(
+      /(?=.*?[0-9])(?=.*?[a-zA-Z])*/,
+      'Need characters including a number and a lower/upper letter.',
+    ),
+  confirmPassword: Yup.string().oneOf(
+    [Yup.ref('password'), null],
+    'Passwords must match',
+  ),
+});
+
 export const SignUp: FC = () => {
-  const [form, setForm] = useState<AccountSignUpTypes>({
-    email: '',
-    username: '',
-    password: '',
-    confirmPassword: '',
+  const {
+    register,
+    handleSubmit,
+    formState,
+    errors,
+  } = useForm<AccountSignUpTypes>({
+    resolver: yupResolver(schema),
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setForm({ ...form, [name]: value });
-  };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(form);
+  const onSubmit = async (data: AccountSignUpTypes) => {
     const reqSignUp = {
       path: '/register',
-      bodyParams: form,
+      bodyParams: data,
     };
+    console.log(reqSignUp);
     await fetcher.post(reqSignUp);
   };
 
@@ -56,45 +78,64 @@ export const SignUp: FC = () => {
           <Box textAlign="center" mb="20px">
             <Heading>Sign Up</Heading>
           </Box>
-          <form onSubmit={handleSubmit}>
-            <FormControl id="email" isRequired>
-              <FormLabel>Email address</FormLabel>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormControl isRequired isInvalid={!!errors.email}>
+              <FormLabel htmlFor="email">Email address</FormLabel>
               <Input
                 name="email"
                 type="email"
                 placeholder="example@aaa.com"
                 autoFocus
-                isInvalid={false}
-                onChange={handleChange}
+                ref={register}
               />
+              <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
             </FormControl>
-            <FormControl id="username" mt="24px" isRequired>
+
+            <FormControl mt="24px" isRequired isInvalid={!!errors.username}>
               <FormLabel>Username</FormLabel>
-              <Input name="username" type="name" onChange={handleChange} />
+              <Input name="username" type="name" ref={register} />
+              <FormErrorMessage>{errors.username?.message}</FormErrorMessage>
             </FormControl>
-            <FormControl id="password" mt="24px" isRequired>
+
+            <FormControl mt="24px" isRequired isInvalid={!!errors.password}>
               <FormLabel>Password</FormLabel>
               <Input
                 name="password"
                 type="password"
                 placeholder="********"
-                onChange={handleChange}
+                ref={register}
               />
+              <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
               <FormHelperText>
-                Make sure it's at least 15 characters OR at least 8 characters
-                including a number and a lowercase letter.
+                Make sure it's at least 8 characters including a number and a
+                lowercase letter.
               </FormHelperText>
             </FormControl>
-            <FormControl id="confirmPassword" mt="24px" isRequired>
+
+            <FormControl
+              mt="24px"
+              isRequired
+              isInvalid={!!errors.confirmPassword}
+            >
               <FormLabel>Confirm Password</FormLabel>
               <Input
                 name="confirmPassword"
                 type="password"
                 placeholder="********"
-                onChange={handleChange}
+                ref={register}
               />
+              <FormErrorMessage>
+                {errors.confirmPassword?.message}
+              </FormErrorMessage>
             </FormControl>
-            <Button w="full" mt="24px" colorScheme="green" type="submit">
+
+            <Button
+              w="full"
+              mt="24px"
+              colorScheme="green"
+              type="submit"
+              isLoading={formState.isSubmitting}
+            >
               Create Account!
             </Button>
           </form>
