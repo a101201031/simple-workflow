a6f0e30e638eca0722ecd987e01d418491c16f79
a101201031 <0825tngus@naver.com>
2021-02-09 00:45:59 +0900

    feat: add theme of chakra-ui

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,18 +2,26 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from 'Home';
 import { SignUp } from 'SignUp';
+import { SignIn } from 'SignIn';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from 'theme';
 
 export const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/sign-up">
-          <SignUp />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <ChakraProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/sign-up">
+            <SignUp />
+          </Route>
+          <Route path="/sign-in">
+            <SignIn />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </ChakraProvider>
   );
 };
