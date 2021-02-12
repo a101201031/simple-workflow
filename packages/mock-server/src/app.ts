f166b46d544346e88b777ba2b9341c458c80aca8
a101201031 <0825tngus@naver.com>
2021-02-13 00:07:57 +0900

    feat: add fake-api(register, token, authorization)

--- packages/mock-server/src/app.ts
+++ packages/mock-server/src/app.ts
@@ -1,19 +1,38 @@
 import jsonServer from 'json-server';
-import { join } from 'path';
+const router = jsonServer.router('./src/data.json');
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
-// const userRegister = jsonServer.router(join(__dirname, ))
 
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 
 const port = process.env.PORT || 8000;
 
-server.post('/register', (req, res) => {
-  res.jsonp({
+server.post('/register', (_, res) =>
+  res.json({
     accessToken: '123123123123',
-  });
+  }),
+);
+
+server.post('/token', (req, res) => {
+  if (req.body.email === 'admin@alwayscoding.app') {
+    res.json({
+      accessToken: '123123123123',
+    });
+  } else {
+    res.status(400).json({ message: 'error' });
+  }
 });
+
+server.use('/', (req, res, next) => {
+  if (req.headers.authorization === 'Bearer 123123123123') {
+    next();
+  } else {
+    res.status(400).json({ message: 'Autorization Error' });
+  }
+});
+
+server.use(router);
 
 server.listen(port, () => {
   console.log('JSON Server is running');
