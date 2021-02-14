ca0d3df73bf2983621cfd58373ff14ce745c88f0
a101201031 <0825tngus@naver.com>
2021-02-14 22:12:45 +0900

    feat: add custom style (Menu)

---
+++ packages/frontend/src/styles/Menu.ts
@@ -0,0 +1,11 @@
+import { Flex } from '@chakra-ui/react';
+import { chakra } from '@chakra-ui/react';
+
+export const SideContents = chakra(Flex, {
+  baseStyle: {
+    px: '16px',
+    py: '0px',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+});
