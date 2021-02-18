d7548e61b532e4f49a91e623ef21860785b18817
a101201031 <0825tngus@naver.com>
2021-02-18 23:43:22 +0900

    feat: add side menu

--- packages/frontend/src/pages/main/Main.tsx
+++ packages/frontend/src/pages/main/Main.tsx
@@ -1,9 +1,23 @@
-import { Box, Flex, Skeleton, Spinner, Text } from '@chakra-ui/react';
-import { fetcher } from 'helper';
-import React, { FC, Suspense, useEffect } from 'react';
+import { Box, Flex, Skeleton, Spacer, Text } from '@chakra-ui/react';
+import React, { FC, Suspense } from 'react';
 import { useHistory } from 'react-router-dom';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { MeAtom, MeSelector } from 'stores';
+import { atom, useRecoilState, useRecoilValue } from 'recoil';
+import { MeSelector } from 'stores';
+import { SideChild, SideContents } from 'styles';
+import {
+  MdKeyboardArrowRight,
+  MdKeyboardArrowDown,
+  MdSubdirectoryArrowRight,
+} from 'react-icons/md';
+
+interface MenuActiveTypes {
+  active: 'appr' | undefined;
+}
+
+const MenuActiveAtom = atom<MenuActiveTypes>({
+  key: 'MenuActiveAtom',
+  default: { active: 'appr' },
+});
 
 export const Main: FC = () => {
   const { push } = useHistory();
@@ -11,28 +25,87 @@ export const Main: FC = () => {
   if (localStorage.getItem('accessToken') !== '123123123123') {
     push('/');
   }
-  // const [me, setMe] = useRecoilState(MeAtom);
-  // const me = useRecoilValue(MeSelector);
+  const [activeMenu, setActiveMenu] = useRecoilState(MenuActiveAtom);
+  const apprClick = (e: React.MouseEvent) => {
+    setActiveMenu({ active: activeMenu.active ? undefined : 'appr' });
+  };
 
   return (
-    <Box bgColor="gray.700" h="100vh" w="320px">
-      <Box h="80px" p="8px" textAlign="right" justifyContent="center">
-        <Suspense fallback={<Skeleton h="100%" />}>
+    <Box bgColor="gray.700" h="100vh" w="240px">
+      <Flex
+        h="80px"
+        p="8px"
+        border="1px solid"
+        borderColor="gray.600"
+        textAlign="right"
+        alignItems="center"
+      >
+        <Suspense fallback={<Skeleton w="100%" h="100%" />}>
           <MyInfo></MyInfo>
         </Suspense>
-      </Box>
-      <Flex flexDirection="column"></Flex>
+      </Flex>
+      <Flex
+        flexDir="column"
+        justifyContent="space-around"
+        alignContent="space-around"
+      >
+        <SideContents>
+          <MdKeyboardArrowRight />
+          <Box w="150px">
+            <Text isTruncated>Main Page</Text>
+          </Box>
+          <Spacer />
+        </SideContents>
+        <SideContents onClick={apprClick}>
+          {activeMenu.active ? (
+            <MdKeyboardArrowDown />
+          ) : (
+            <MdKeyboardArrowRight />
+          )}
+          <Box w="150px">
+            <Text isTruncated>Appr. Management</Text>
+          </Box>
+          <Spacer />
+        </SideContents>
+        <SideChild>
+          <MdSubdirectoryArrowRight />
+          <Box w="150px">
+            <Text isTruncated>Request for Appr.</Text>
+          </Box>
+          <Spacer />
+        </SideChild>
+        <SideChild>
+          <MdSubdirectoryArrowRight />
+          <Box w="150px">
+            <Text isTruncated>Apprroval</Text>
+          </Box>
+          <Spacer />
+        </SideChild>
+        <SideChild>
+          <MdSubdirectoryArrowRight />
+          <Box w="150px">
+            <Text isTruncated>Appr. history</Text>
+          </Box>
+          <Spacer />
+        </SideChild>
+        <SideContents>
+          <MdKeyboardArrowRight />
+          <Box w="150px">
+            <Text isTruncated>Organization Chart</Text>
+          </Box>
+          <Spacer />
+        </SideContents>
+      </Flex>
     </Box>
   );
 };
 
 const MyInfo: FC = () => {
   const me = useRecoilValue(MeSelector);
-
   return (
-    <>
-      <Text>{me.groupName || 'None Group.'}</Text>
-      <Text>{me.userName}</Text>
-    </>
+    <Box h="100%" w="100%">
+      <Text>{me.group?.groupName || 'None Group.'}</Text>
+      <Text fontWeight="bold">{me.userName}</Text>
+    </Box>
   );
 };
--- packages/frontend/src/styles/Menu.ts
+++ packages/frontend/src/styles/Menu.ts
@@ -1,9 +1,19 @@
-import { Flex } from '@chakra-ui/react';
-import { chakra } from '@chakra-ui/react';
+import { chakra, Flex } from '@chakra-ui/react';
 
 export const SideContents = chakra(Flex, {
   baseStyle: {
     px: '16px',
+    py: '0px',
+    h: '40px',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+});
+
+export const SideChild = chakra(Flex, {
+  baseStyle: {
+    pl: '32px',
+    pr: '16px',
     py: '0px',
     h: '40px',
     alignItems: 'center',
