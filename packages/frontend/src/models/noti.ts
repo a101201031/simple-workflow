3cbdcaea98d9cbe560264e5afed978f9461d9289
a101201031 <0825tngus@naver.com>
2021-03-07 21:52:42 +0900

    feat: fix model type (Noti)

--- packages/frontend/src/models/noti.ts
+++ packages/frontend/src/models/noti.ts
@@ -1,10 +1,11 @@
 import { User } from './user';
 
 export interface Noti {
-  _id?: number;
+  id: number;
   user: User;
   isRead: Boolean;
   notiMessage: String;
+  notiUrl: String;
   insertDate: Date;
   readDate: Date | null;
 }
